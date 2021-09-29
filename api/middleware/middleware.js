717a1fdb73f91e3bd90c82afa850f162d85ee077
NeNeTheCreative <shecodes@nenethecreative.com>
2021-09-29 16:43:35 -0400

    validateUser done

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   const timestamp = new Date().toLocaleString()
   const method = req.method
@@ -6,16 +8,37 @@ function logger(req, res, next) {
   next()
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
-  console.log('validateUserId middleware')
-next()
+  try {
+    const {id} = req.params
+    const user = await User.getById(id)
+    if (!user) {
+      res.status(404).json({
+        message: "user not found"
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch(err) {
+    res.status(500).json({
+      message: "cannot find user"
+    })
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('validateUser middleware')
-next()
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: "missing required name field"
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
+ 
 }
 
 function validatePost(req, res, next) {
